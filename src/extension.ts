51d2afb98c64c3f70aa18a8b047a325f95d438cf
damolinx <damolinx>
2022-05-19 21:23:55 -0700

    Separate `Bookmark Editor` into two visible commands: `Bookmark Editor (global)` and  `Bookmark Editor (workspace)` so keybinding is possible.

--- src/extension.ts
+++ src/extension.ts
@@ -23,34 +23,36 @@ export function activate(context: vscode.ExtensionContext) {
   // Register Commands
   context.subscriptions.push(
     vscode.commands.registerCommand(
-      "bookmarks.addBookmark",
-      async (pathOrGroup?: string | vscode.Uri | BookmarkGroup, kind?: BookmarkKind) => {
-        let uri: vscode.Uri | undefined;
-
-        if (pathOrGroup instanceof vscode.Uri) {
-          uri = pathOrGroup;
-        } else if (typeof pathOrGroup === "string") {
-          uri = vscode.Uri.parse(pathOrGroup);
-        } else {
-          kind = (pathOrGroup instanceof BookmarkGroup)
-            ? pathOrGroup.kind
-            : vscode.workspace.workspaceFolders?.length ? 'workspace' : 'global';
-          uri = vscode.window.activeTextEditor?.document.uri;
-        }
-
+      "_bookmarks.addBookmark",
+      async (pathOrUri: string | vscode.Uri | undefined, kind: BookmarkKind) => {
+        const uri: vscode.Uri | undefined = (pathOrUri instanceof vscode.Uri)
+          ? pathOrUri
+          : (typeof pathOrUri === "string")
+            ? vscode.Uri.parse(pathOrUri)
+            : vscode.window.activeTextEditor?.document.uri;
         if (uri) {
           await bookmarkManager.addBookmarkAsync(uri, kind);
-        } else {
-          vscode.window.showWarningMessage("Found no open editor nor received a URI to bookmark");
         }
       }),
+    vscode.commands.registerCommand(
+      "bookmarks.addBookmark.global",
+      (pathOrUri?: string | vscode.Uri) => 
+        vscode.commands.executeCommand("_bookmarks.addBookmark", pathOrUri, 'global')),
+    vscode.commands.registerCommand(
+      "bookmarks.addBookmark.tree",
+      (group: BookmarkGroup) => 
+        vscode.commands.executeCommand("_bookmarks.addBookmark", undefined, group.kind)),
+    vscode.commands.registerCommand(
+      "bookmarks.addBookmark.workspace",
+      (pathOrUri?: string | vscode.Uri) => 
+        vscode.commands.executeCommand("_bookmarks.addBookmark", pathOrUri, 'workspace')),
     vscode.commands.registerCommand(
       "bookmarks.removeBookmark",
       (bookmark: Bookmark) => bookmarkManager.removeBookmarkAsync(bookmark)),
     vscode.commands.registerCommand(
       "bookmarks.removeAllBookmarks",
-      async (group?: BookmarkKind | BookmarkGroup) => {
-        const kind = (group instanceof BookmarkGroup) ? group.kind : group;
+      async (groupOrKind?: BookmarkKind | BookmarkGroup) => {
+        const kind = (groupOrKind instanceof BookmarkGroup) ? groupOrKind.kind : groupOrKind;
         if (bookmarkManager.hasBookmarks(kind)) {
           const option = await vscode.window.showInformationMessage("Are you sure you want to remove all bookmarks?", "Yes", "No");
           if (option === "Yes") {
