96bd77066a8c1ee775531100ebee01f53205203b
Daniel Molina <damolina@outlook.com>
2025-03-31 21:38:19 -0700

    Update build targets

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,16 +1,29 @@
 import * as esbuild from 'esbuild';
+import * as fs from 'fs';
 
-esbuild.build({
-  entryPoints: ['./src/extension.ts'],
-  bundle: true,
-  minify: true,
-  sourcemap: true,
-  platform: 'node',
-  target: 'es2022',
-  outfile: './out/extension.js',
-  external: ['vscode'],
-}).catch(() => {
+const production = process.argv.includes('--production');
+
+async function main() {
+  const context = await esbuild.context({
+    entryPoints: ['./src/extension.ts'],
+    external: ['vscode'],
+    outfile: './out/extension.js',
+    platform: 'node',
+
+    bundle: true,
+    minify: production,
+    sourcemap: !production,
+    sourcesContent: false,
+  });
+
+  if (production) {
+    fs.rmSync('./out', { recursive: true, force: true });
+  }
+  await context.rebuild();
+  await context.dispose();
+}
+
+main().catch((e) => {
   console.error(e);
   process.exit(1);
-}
-);
+});
