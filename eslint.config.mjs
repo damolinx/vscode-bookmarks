3d78a1aed014d64977466812593de494aabaf907
Daniel Molina <damolina@outlook.com>
2025-08-03 15:18:03 -0700

    Update linting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -40,7 +40,8 @@ export default tseslint.config(
       '@typescript-eslint/no-unused-vars': [
         'error',
         {
-          'argsIgnorePattern': '^_'
+          'argsIgnorePattern': '^_.*',
+          'varsIgnorePattern': '^_.*'
         }
       ],
     }
