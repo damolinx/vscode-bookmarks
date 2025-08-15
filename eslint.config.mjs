f608c7c30ecfdc1454d9ef694ff2e6fdd931b664
Daniel Molina <damolina@outlook.com>
2025-08-15 10:09:18 -0700

    Update deps  / prettier rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,8 @@
  */
 // @ts-check
 import js from '@eslint/js';
+import eslintImport from 'eslint-plugin-import';
+import eslintPrettier from 'eslint-plugin-prettier';
 import tseslint from 'typescript-eslint';
 import stylistic from '@stylistic/eslint-plugin';
 
@@ -13,22 +15,47 @@ export default tseslint.config(
     ignores: [
       '.vscode-test',
       'out',
-    ]
+    ],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   ...tseslint.configs.stylistic,
   {
     plugins: {
-      '@stylistic': stylistic
+      '@stylistic': stylistic,
+      '@eslint/import': eslintImport,
+      '@eslint/prettier': eslintPrettier,
     },
     rules: {
       'arrow-parens': ['error', 'always'],
       'curly': 'warn',
       'no-trailing-spaces': 'error',
-      '@stylistic/comma-dangle': ["error", "always-multiline"],
-      '@stylistic/quotes': ['error', 'single'],
-      '@stylistic/semi': ['error', 'always'],
+      '@eslint/import/order': [
+        'error',
+        {
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          'newlines-between': 'never',
+          pathGroups: [
+            {
+              pattern: 'vscode',
+              group: 'builtin',
+              position: 'before',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+        },
+      ],
+      '@eslint/prettier/prettier': [
+        'error',
+        {
+          'semi': true,
+          'singleQuote': true,
+          'printWidth': 100,
+          'tabWidth': 2,
+        }
+      ],
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
       '@typescript-eslint/naming-convention': [
         'warn',
         {
@@ -37,6 +64,7 @@ export default tseslint.config(
         }
       ],
       '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': [
         'error',
         {
